2aac5e3e36e26cf4681a24be08d06934a54ad6c3
MohammedGhazaly <warendragon67@gmail.com>
2023-09-18 19:09:17 +0200

    Added calender

--- lib/tabs/taks_list/tasks_list.dart
+++ lib/tabs/taks_list/tasks_list.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:calendar_agenda/calendar_agenda.dart';
+import 'package:todo_app/my_theme.dart';
 
 class TasksTab extends StatelessWidget {
   const TasksTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        CalendarAgenda(
+          selectedDateColor: MyTheme.primaryColor,
+          locale: "en",
+          events: [],
+          initialDate: DateTime.now(),
+          firstDate: DateTime.now().subtract(const Duration(days: 365)),
+          lastDate: DateTime.now().add(const Duration(days: 365)),
+          onDateSelected: (date) {},
+        )
+      ],
+    );
   }
 }
